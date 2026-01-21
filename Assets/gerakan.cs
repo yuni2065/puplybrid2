e00cd6ab185b860d6ddd69f12a3b6c5c1291e77c
yuni2065 <yuniresa2065@gmail.com>
2026-01-21 20:22:52 +0800

    Initial commit

---
+++ Assets/gerakan.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gerakan : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D Rb;
+    public float jumpForce;
+    float score;
+
+
+    public Text scoreTxt;
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreTxt.text = "" + score;
+        if (Input.GetMouseButtonDown(0))
+        {
+            Rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "point")
+        {
+            score++;
+        }
+        if(collision.gameObject.tag =="pipa")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/pipescript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pipescript : MonoBehaviour
+{
+    float nilaiRandom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nilaiRandom = Random.Range(-2.72f, 2.40f);
+        transform.position = new Vector2(transform.position.x, nilaiRandom);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, Vector2.left * 100, Time.deltaTime * 5);
+    }
+}
